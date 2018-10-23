89bdd5cee56726b5052d7acd3cd83194f2417ae6
PolliHam <polina.shenets@gmail.com>
2018-10-24 02:39:12 +0300

    /sum

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const http = require('http');
 
+const readAll = require('./read_all');
+const readArticle = require('./read_article');
+const createArticle = require('./create_article');
+const updateArticle = require('./update_article');
+const deleteArticle = require('./delete_article');
+const createComment = require('./create_comment');
+const deleteComment = require('./delete_comment');
+const helper = require('./helper');
+const sum = require('./sum');
+
+
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const handlers = {
-    '/sum': sum
+    '/sum': sum,
+    '/api/articles/readAll': readAll,
+    '/api/articles/read': readArticle,
+    '/api/articles/createArticle': createArticle,
+    '/api/articles/updateArticle': updateArticle,
+    '/api/articles/deleteArticle': deleteArticle,
+    '/api/comments/createComment': createComment,
+    '/api/comments/deleteComment': deleteComment
 };
 
 const server = http.createServer((req, res) => {
@@ -16,12 +34,10 @@ const server = http.createServer((req, res) => {
                 res.statusCode = err.code;
                 res.setHeader('Content-Type', 'application/json');
                 res.end( JSON.stringify(err) );
-
-                return;
             }
-
+            helper.updateArticles();
+            helper.logger(req.url,payload);
             res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
             res.end( JSON.stringify(result) );
         });
     });
@@ -35,26 +51,17 @@ function getHandler(url) {
     return handlers[url] || notFound;
 }
 
-function sum(req, res, payload, cb) {
-    const result = { c: payload.a + payload.b };
-
-    cb(null, result);
-}
-
 function notFound(req, res, payload, cb) {
     cb({ code: 404, message: 'Not found'});
 }
 
 function parseBodyJson(req, cb) {
     let body = [];
-
     req.on('data', function(chunk) {
         body.push(chunk);
     }).on('end', function() {
         body = Buffer.concat(body).toString();
-
         let params = JSON.parse(body);
-
         cb(null, params);
     });
 }
